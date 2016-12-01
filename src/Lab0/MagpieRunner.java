710d37342adcde7463c0acbf946d7632498b100b
seankelker <seankelker@gmail.com>
2016-12-01 02:44:08 -0800

    12/1/16

--- src/Lab0/MagpieRunner.java
+++ src/Lab0/MagpieRunner.java
@@ -4,10 +4,10 @@ import java.util.Scanner;
 
 /**
  * A simple class to run the Magpie class.
- * @author Laurie White
- * @version April 2012
+ * @author Sean Kelker
+ * @version Dec 1
  */
-public class MagpieRunner2
+public class MagpieRunner
 {
 
 	/**
@@ -15,12 +15,13 @@ public class MagpieRunner2
 	 */
 	public static void main(String[] args)
 	{
-		Magpie2 maggie = new Magpie2();
+		Magpie maggie = new Magpie();
+		
 		System.out.println (maggie.getGreeting());
 		Scanner in = new Scanner (System.in);
 		String statement = in.nextLine();
 		
-		while (!statement.equals("Bye"))
+		while (!statement.equalsIgnoreCase("Bye"))
 		{
 			System.out.println (maggie.getResponse(statement));
 			statement = in.nextLine();
