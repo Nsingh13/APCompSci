811cc84ad14aeb86622677e857af51e7c5e819b3
seankelker <seankelker@gmail.com>
2016-12-05 14:14:19 -0800

    12/5/16

--- src/Assignmnet25/Assignment25.java
+++ src/Assignmnet25/Assignment25.java
@@ -1,28 +1,18 @@
 package Assignmnet25;
 
 public class Assignment25 {
-
     public static void main(String[] args) {
 	int students = 100;
-	int lockers = students;
-
 	boolean[] lockersOpen = new boolean[students + 1];
-
-	for (int i = 1; i < students + 1; i++) {
+	for (int i = 1; i <= students + 1; i++) {
 	    for (int j = i; j <= (students); j += i) {
 		lockersOpen[j] = !lockersOpen[j];
-
-		//System.out.println(lockersOpen[i] + " J:" + j + " I:" + i);
 	    }
-
 	}
-	for (int i = 0; i < students; i++) {
+	for (int i = 0; i <= students; i++) {
 	    if (lockersOpen[i] == true) {
 		System.out.println(i);
-
 	    }
 	}
-
     }
-
 }
