f2d1bd21a0b1f448dab99e45315d6d17dacb719c
seankelker <seankelker@gmail.com>
2017-01-18 20:56:09 -0800

    1/18/17

--- src/Assignment28/Board.java
+++ src/Assignment28/Board.java
@@ -80,4 +80,5 @@ public class Board {
 		return winner;
 
 	}
+	
 }
--- src/Assignment28/Runner.java
+++ src/Assignment28/Runner.java
@@ -1,22 +1,25 @@
 package Assignment28;
 
+import java.util.Random;
 import java.util.Scanner;
 
 public class Runner {
 
+	private static boolean isPlayer1 = true;
+	private static boolean twoPlayer;
+	private static int loc;
+
+	private static String[][] playBoard = new String[3][3];
+	private static String x = "X";
+	private static String o = "O";
+
+	private static Scanner scan = new Scanner(System.in);
+	private static Board board = new Board(playBoard);
+	private static Random rand = new Random();
+	private static Player player1 = new Player(x);
+	private static Player player2 = new Player(o);
+
 	public static void main(String[] args) {
-
-		Scanner scan = new Scanner(System.in);
-		String[][] playBoard = new String[3][3];
-		Board board = new Board(playBoard);
-		String x = "X";
-		String o = "O";
-		int loc;
-
-		Player player1 = new Player(x);
-		Player player2 = new Player(o);
-
-		boolean isPlayer1 = true;
 
 		System.out.println("Welcome to Tic Tac Toe");
 
@@ -28,30 +31,34 @@ public class Runner {
 			if (isPlayer1) {
 				System.out.println("");
 				System.out.print("Player1's turn to move:");
-				loc = scan.nextInt();
-				System.out.println("");
-				player1.move(loc, board);
+				runTurn(player1);
+
 				if (board.determineWinner()) {
+
 					System.out.println("Player 1 wins ");
-					break;
 				}
 
 			} else {
 				System.out.println("");
 				System.out.print("Player2's turn to move:");
-				loc = scan.nextInt();
-				System.out.println("");
-				player2.move(loc, board);
+				runTurn(player2);
+
 				if (board.determineWinner()) {
+
 					System.out.println("Player 2 wins ");
-					break;
 				}
 
 			}
 
 			isPlayer1 = !isPlayer1;
-
 		}
+	}
+
+	public static void runTurn(Player p) {
+
+		loc = scan.nextInt();
+		System.out.println("");
+		p.move(loc, board);
 
 	}
 
