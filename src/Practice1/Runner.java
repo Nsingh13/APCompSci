e0e9b2116cd4393a9a5f1d1f592d0323472b1045
seankelker <seankelker@gmail.com>
2017-03-01 09:35:59 -0800

    3/1/17

--- src/Practice1/Runner.java
+++ src/Practice1/Runner.java
@@ -1,13 +1,22 @@
 package Practice1;
 
+import java.util.List;
+
 public class Runner {
+	public static LogMessage log1 = new LogMessage("CLient3:Key Lets test this");
+	public static LogMessage log2 = new LogMessage("CLient3:Lets test this");
+	public static LogMessage log3 = new LogMessage("CLient3:Key Lets test this");
 
 	public static void main(String[] args) {
 
-		LogMessage log = new LogMessage("CLient3:KeyLets test this");
-		System.out.println(log.getMachineId());
-		System.out.println(log.getDescription());
-		System.out.println(log.contatinsWord("Key"));
+		SystemLog system = new SystemLog();
+
+		List<LogMessage> test = system.removeMessages("Key");
+
+		for (int i = 0; i < test.size(); i++) {
+			System.out.println(test.get(i).getDescription());
+		}
+
 	}
 
 }
--- src/Practice1/SystemLog.java
+++ src/Practice1/SystemLog.java
@@ -1,5 +1,33 @@
 package Practice1;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class SystemLog {
+
+	private List<LogMessage> messageList = new ArrayList<LogMessage>();
+	private List<LogMessage> removedList = new ArrayList<LogMessage>();
+
+	public SystemLog() {
+		messageList.add(Runner.log1);
+		messageList.add(Runner.log2);
+		messageList.add(Runner.log3);
+	}
+
+	public List<LogMessage> removeMessages(String keyword) {
+
+		for (int i = 0; i < messageList.size(); i++) {
+			if ((messageList.get(i).contatinsWord(keyword))) {
+
+				removedList.add(messageList.get(i));
+				messageList.remove(i);
+
+			}
+
+		}
+
+		return removedList;
+
+	}
 
 }
